b1f9ce0be3b9021ef6b9452b6913c7596cc20a35
CarlosHaroInfante <145343573+CarlosHaroInfante@users.noreply.github.com>
2024-03-06 19:45:35 +0100

    Add files via upload

---
+++ Ejercicio3JS/src/main/webapp/js.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+function hola(){
+ let array1  = [0, 1];
+ 
+ const array2 = ["PEPE", 33];
+ 
+ array1[0] = prompt("Primer valor");
+ array1[1] = prompt("Segundo valor");
+
+alert(array1[0] + array1[1]);
+alert(array2[0] + array2[1]);
+ }
